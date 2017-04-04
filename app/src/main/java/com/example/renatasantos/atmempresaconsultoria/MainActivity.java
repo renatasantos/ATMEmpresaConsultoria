756dac0814aeb45371512293728665ace436545e
Renata Santos <renata.miriam08@gmail.com>
2017-04-04 14:51:15 -0300

    Inicial Commit

---
+++ app/src/main/java/com/example/renatasantos/atmempresaconsultoria/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.renatasantos.atmempresaconsultoria;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView botaoEmpresa;
+    private ImageView botaoServicos;
+    private ImageView botaoCliente;
+    private ImageView botaoContato;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        botaoEmpresa = (ImageView) findViewById(R.id.empresaId);
+        botaoServicos = (ImageView) findViewById(R.id.servicosId);
+        botaoCliente = (ImageView) findViewById(R.id.clienteId);
+        botaoContato = (ImageView) findViewById(R.id.contatoId);
+
+        botaoEmpresa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, EmpresaActivity.class));
+
+            }
+        });
+
+        botaoServicos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ServicosActivity.class));
+            }
+        });
+
+        botaoCliente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,ClienteActivity.class));
+            }
+        });
+
+        botaoContato.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,ContatoActivity.class));
+            }
+        });
+
+
+    }
+}
